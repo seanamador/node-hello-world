402cb82d31fb30befba22a913900a144e9b2d139
seanamador <amador.sean@gmail.com>
2014-09-18 14:47:02 +0800

    zcbgrdgh

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ var http = require('http')
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
  //   res.end('Hello World Philippines Agsib');
+  
 //});
- 
+  //	.
+});
+
 server.listen(5000);
 
 console.log("server running at http://0.0.0.0:5000");
