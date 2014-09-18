15608105d7582e764ec27eda537ebef1c0761eeb
seanamador <amador.sean@gmail.com>
2014-09-18 14:43:27 +0800

    qwevbn

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var http = require('http')
 
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World Philippines Agsib');
-});
+ //   res.end('Hello World Philippines Agsib');
+//});
 
 server.listen(5000);
 
